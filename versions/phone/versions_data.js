22f5fc12a5c0e762193dca1b69aa8367b329db68
Haniyeh Sam <samhani85@gmail.com>
2025-05-11 22:10:10 +0200

    Phone new ver

---
+++ versions/phone/versions_data.js
@@ -0,0 +1,65 @@
+const versions = [
+  {
+    title: "Dot’s Big Debut",
+    date: "2025-01-09 21:48",
+    desc: "Tap the dot to begin.",
+    img: "250109_2148.png",
+    folder: "250109_2148"
+  },
+  {
+    title: "Slide into the Spotlight",
+    date: "2025-01-16 20:39",
+    desc: "Dot now slides in.",
+    img: "250116_2039.png",
+    folder: "250116_2039"
+  },
+  {
+    title: "Chaos Carnival",
+    date: "2025-01-23 20:13",
+    desc: "Sound, multi-dots, chaos!",
+    img: "250123_2013.png",
+    folder: "250123_2013"
+  },
+  {
+    title: "Let's Get More Abstract",
+    date: "2025-02-15 00:06",
+    desc: "Minimalist UI journey.",
+    img: "250215_0006.png",
+    folder: "250215_0006"
+  },
+  {
+    title: "Menu Momentum",
+    date: "2025-02-20 20:51",
+    desc: "Main menu begins.",
+    img: "250220_2051.png",
+    folder: "250220_2051"
+  },
+  {
+    title: "Intermission Overload",
+    date: "2025-02-24 21:04",
+    desc: "Mid-game breaks added.",
+    img: "250224_2104.png",
+    folder: "250224_2104"
+  },
+  {
+    title: "Zen? Nah!",
+    date: "2025-02-26 21:14",
+    desc: "Zen mode removed.",
+    img: "250226_2114.png",
+    folder: "250226_2114"
+  },
+  {
+    title: "JSON Reboot",
+    date: "2025-02-27 20:34",
+    desc: "JSON takes over.",
+    img: "250227_2034.png",
+    folder: "250227_2034"
+  },
+  {
+    title: "Vertical Vibes",
+    date: "2025-03-03 20:41",
+    desc: "Portrait perfected.",
+    img: "250303_2041.png",
+    folder: "250303_2041"
+  }
+];
