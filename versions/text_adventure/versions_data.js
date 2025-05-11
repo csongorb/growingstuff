a7855a54a331c4a41eb846cea5874b3cd300f7b0
csongorb <csongorb@gmail.com>
2025-05-11 15:05:53 +0200

    Moved data to an extra js-file.

---
+++ versions/text_adventure/versions_data.js
@@ -0,0 +1,7 @@
+
+
+
+
+const data = [
+    { title: "Kick the dog", date: "2024-09-18", changes: ["pippin sat somewhere","pippin did something","pippin probably ate food"], url: "./240918_0414/index.html" }
+  ];
