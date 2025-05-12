9b57619b4cb57d363bb89e37407f9fdcaa15a23b
csongorb <csongorb@gmail.com>
2025-05-12 15:53:52 +0200

    Updated version_data for Phone (still not finished).

--- versions/phone/versions_data.js
+++ versions/phone/versions_data.js
@@ -1,34 +1,34 @@
 const versions = [
   {
-    title: "Dot’s Big Debut",
+    title: "The most basic tap simulator",
     date: "2025-01-09 21:48",
     desc: "Tap the dot to begin.",
     img: "250109_2148.png",
     folder: "250109_2148"
   },
   {
-    title: "Slide into the Spotlight",
+    title: "Swipe prototyping",
     date: "2025-01-16 20:39",
     desc: "Dot now slides in.",
     img: "250116_2039.png",
     folder: "250116_2039"
   },
   {
-    title: "Chaos Carnival",
+    title: "Basic keyboard mode",
     date: "2025-01-23 20:13",
     desc: "Sound, multi-dots, chaos!",
     img: "250123_2013.png",
     folder: "250123_2013"
   },
   {
-    title: "Let's Get More Abstract",
-    date: "2025-02-15 00:06",
+    title: "Basic 'Phoning' interaction in place",
+    date: "2025-02-17 19:30",
     desc: "Minimalist UI journey.",
     img: "250215_0006.png",
-    folder: "250215_0006"
+    folder: "250217_1930"
   },
   {
-    title: "Menu Momentum",
+    title: "Better drag and tutorial",
     date: "2025-02-20 20:51",
     desc: "Main menu begins.",
     img: "250220_2051.png",
@@ -56,7 +56,7 @@ const versions = [
     folder: "250227_2034"
   },
   {
-    title: "Vertical Vibes",
+    title: "Release",
     date: "2025-03-03 20:41",
     desc: "Portrait perfected.",
     img: "250303_2041.png",
