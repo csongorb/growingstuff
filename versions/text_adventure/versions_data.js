fe44a435cb9fee15b8cb24ad1863efd154760fd6
csongorb <csongorb@gmail.com>
2025-05-13 09:15:54 +0200

    Updated versions for Text Adventure.
    Hopefully more suited to The Designer is Present.

--- versions/text_adventure/versions_data.js
+++ versions/text_adventure/versions_data.js
@@ -2,8 +2,19 @@
 const data = [
     { title: "Kick the dog", 
         date: "2024-09-18", 
-        changes: ["pippin sat somewhere",
-            "pippin did something",
-            "pippin probably ate food"], 
-        url: "./240918_0414/index.html" }
+        changes: ["A very basic framework",
+            "Interactions not working"
+        ], 
+        url: "./240918_0414/index.html" 
+    },
+    { title: "> Journal", 
+        date: "now", 
+        changes: ["Including the most recent updates"], 
+        url: "./journal/index.html" 
+    },
+    { title: "> Live", 
+        date: "now", 
+        changes: ["Newest version"], 
+        url: "./now/index.html" 
+    }
   ];
